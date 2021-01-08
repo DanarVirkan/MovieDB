661c469b0e63e34ea64aa50ee5ccdca7e78e5aab
DanarVirkan <danarvirkan@gmail.com>
2021-01-08 20:58:13 +0700

    Removed lint module

--- app/src/main/java/com/moviedb/MainActivity.kt
+++ app/src/main/java/com/moviedb/MainActivity.kt
@@ -3,7 +3,6 @@ package com.moviedb
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.moviedb.databinding.ActivityMainBinding
